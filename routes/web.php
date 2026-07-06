c3a01b7d99caaebecc98f388a205bd3042295036
verenasuna <verenasuna80@gmail.com>
2026-07-06 11:55:29 +0700

    uas pwl

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,46 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\JurusanController;
 use App\Http\Controllers\MatakuliahController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KelasController;
+use App\Http\Controllers\KRSController;
+use App\Http\Controllers\KRSDetailController;
 
-Route::get('/welcome', function () {
-    return view('welcome    ');
+Route::get('/', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/login', [AuthController::class, 'loginView'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'registerView'])->name('register.view');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+
+    Route::resource('/mahasiswa', MahasiswaController::class);
+
+    Route::resource('/dosen', DosenController::class);
+
+    Route::resource('/jurusan', JurusanController::class);
+
+    Route::resource('/mata_kuliah', MatakuliahController::class);
+
+    Route::resource('/kelas', KelasController::class)
+        ->except(['show', 'edit', 'update']);
+
+    Route::resource('/krs', KRSController::class);
+
+    Route::resource('/krs-detail', KRSDetailController::class);
+
+    Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
 });
-
-Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
-Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show']);
-Route::get('/mahasiswa-create', [MahasiswaController::class, 'create'])->name('mahasiswa.add');
-Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->name('mahasiswa.save');
-Route::get('/mahasiswa-edit/{id}', [MahasiswaController::class, 'edit'])->name('mahasiswa.update');
-Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.edit');
-Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.delete');
-
-
-
-
-Route::get('/dosen', [DosenController::class, 'index']);
-Route::get('/dosen/{id}', [DosenController::class, 'show']);
-Route::get('/dosen-create', [DosenController::class, 'create'])->name('dosen.add');
-Route::post('/dosen', [DosenController::class, 'store'])->name('dosen.save');
-Route::get('/dosen-edit/{id}', [DosenController::class, 'edit'])->name('dosen.update');
-Route::put('/dosen/{id}', [DosenController::class, 'update'])->name('dosen.edit');
-Route::delete('/dosen/{id}', [DosenController::class, 'destroy'])->name('dosen.delete');
-
-
-Route::get('/jurusan', [JurusanController::class, 'index']);
-Route::get('/jurusan/{id}', [JurusanController::class, 'show']);
-Route::get('/jurusan-create', [JurusanController::class, 'create'])->name('jurusan.add');
-Route::post('/jurusan', [JurusanController::class, 'store'])->name('jurusan.save');
-Route::get('/jurusan-edit/{id}', [JurusanController::class, 'edit'])->name('jurusan.update');
-Route::put('/jurusan/{id}', [JurusanController::class, 'update'])->name('jurusan.edit');
-Route::delete('/jurusan/{id}', [JurusanController::class, 'destroy'])->name('jurusan.delete');
-
-
-Route::get('/matakuliah', [MatakuliahController::class, 'index']);
-Route::get('/matakuliah/{id}', [MatakuliahController::class, 'show']);
-Route::get('/matakuliah-create', [MatakuliahController::class, 'create'])->name('matakuliah.add');
-Route::post('/matakuliah', [MatakuliahController::class, 'store'])->name('matakuliah.save');
-Route::get('/matakuliah-edit/{id}', [MatakuliahController::class, 'edit'])->name('matakuliah.update');
-Route::put('/matakuliah/{id}', [MatakuliahController::class, 'update'])->name('matakuliah.edit');
-Route::delete('/matakuliah/{id}', [MatakuliahController::class, 'destroy'])->name('matakuliah.delete');
